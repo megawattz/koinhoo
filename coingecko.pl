73d5ec8b930ea85bc77d47ec19fd08a456a1d901
Walt Howard <walterhoward@gmail.com>
2018-04-29 14:30:11 -0400

    enable json output with coingecko.pl

--- coingecko.pl
+++ coingecko.pl
@@ -8,7 +8,7 @@ use Data::Dumper;
 #use POSIX;
 use IO::Socket;
 #use Cwd;
-#use JSON;
+use JSON;
 #use FindBin;
 #use Carp;
 #use Digest::SHA qw(hmac_sha256_hex hmac_sha512_hex);
@@ -27,6 +27,28 @@ sub Output {
   my $priority = shift;
   return if $priority > $Config->{Options}->{verbosity};
   print @_;
+}
+
+sub Commaize {
+    my @rval = ();
+    for my $v (@_) {
+	my $r = reverse($v);
+	$r =~ s/(\d\d\d)/$1,/g;
+	$r = reverse($r);
+	$r =~ s/^,//;
+	$r =~ s/\.,/./;
+	push(@rval, $r);
+    }
+    return @rval;
+}
+
+sub FixUp {
+    my ($values, $remove) = @_;
+    for my $v (@$values) {
+	$v =~ s/$remove//g;
+	$v = "0" if $v eq "";
+    }
+    return $values;
 }
 
 foreach my $arg (@ARGV) {
@@ -55,8 +77,9 @@ my $Request = HTTP::Request->new(GET => $Url);
 my $Response = $Browser->request($Request);
 
 Output(4, "Raw Response:\n", Dumper($Response));
+my $Columns = ['COIN','PRICE','24H','7D','30D','MKT CAP','LIQUIDITY','DEVELOPER','COMMUNITY','PUBLIC INTEREST','TOTAL'];  # only extract columns with these names (in the table header)
 
-my $Extractor = HTML::TableExtract->new(headers => $Config->{Options}->{columns});
+my $Extractor = HTML::TableExtract->new(headers => $Columns);
 
 Output(5, "Extractor:".Dumper($Extractor));
 
@@ -74,35 +97,54 @@ Output(3, "Table:".Dumper($Table));
 
 my $index = 0;
 
-sub Trim {
-    my ($values, $remove) = @_;
-    for my $v (@$values) {
-	$v =~ s/$remove//g;
+if ($Config->{Options}->{format} eq "flat") 
+{
+    my $D = $Config->{Options}->{delimiter};
+    
+    Output(1, sprintf("%8s$D%10s$D%10s$D%10s$D%10s$D%15s$D%15s$D%9s$D%9s$D%9s$D%s$D%s\n", 'Symbol','Price','Change24h','Change7d','Change30d','MarketCap','Liquidity','Developer','Community','PublicInterest','Total','Name'));
+    
+    foreach my $columns ($Table->rows) {
+	my @names = $columns->[0] =~ /(\w.+?)[\r\n]/g;
+	next if not $names[1];
+	$names[1] =~ s/\s/_/g;
+	shift @$columns, 0;
+	unshift(@$columns, $names[0]);
+	push(@$columns, $names[1]);
+	$columns = FixUp($columns, qr/[\s,\$%\*]/);
+	eval {
+	    my $line = sprintf("%8s$D%10.3f$D%10.2f$D%10.2f$D%10.2f$D%15d$D%15d$D%9d$D%9d$D%9d$D%9d$D%s\n", @$columns);
+	    Output(1, $line);
+	};
     }
-    return $values;
 }
 
-my $Tree = {};
+if ($Config->{Options}->{format} eq "json") 
+{
+    my $Tree = {};
+    my @Fields = ('Price', 'Change24h', 'Change7d', 'Change30d',
+		  'MarketCap', 'Liquidity', 'Developer', 'Community',
+		  'PublicInterest', 'Total', 'Name');
+    
+    foreach my $row ($Table->rows) {
+	my @names = $row->[0] =~ /(\w.+?)[\r\n]/g;
+	next if not $names[1];
+	$names[1] =~ s/\s/_/g;
+	my $symbol = shift @$row, 0; # remote symbol from row (it will become the key)
+	push(@$row, $names[1]); #add fullname of coin at end of attributes
+	$row = FixUp($row, qr/[\s,\$%\*]/);
+	
+	my $attributes = {};
+	my $index = 0;
+	
+	foreach my $field (@Fields) {
+	    $attributes->{$field} = $row->[$index++];
+	}
+	
+	$Tree->{$names[0]} = $attributes;
+    }
 
-my $D = $Config->{Options}->{delimiter};
-
-Output(1, sprintf("%8s$D%10s$D%10s$D%10s$D%10s$D%15s$D%15s$D%9s$D%9s$D%9s %s\n", 
-		  'Symbol','Price','Change24h','Change7d','Change30d','MarketCap','Liquidity','Developer','Community','PublicInterest','Total','Name'));
-
-foreach my $columns ($Table->rows) {
-    my @names = $columns->[0] =~ /(\w.+?)[\r\n]/g;
-    $names[1] = $names[1] =~ s/./_/g;
-    #print "NAMES:",join(':', @names),":\n";
-    shift @$columns, 0;
-    unshift(@$columns, $names[0]);
-    #$names[2] =~ s/\s+/_/g;
-    push(@$columns, $names[0]);
-    my $columns = Trim($columns, qr/[\s,\$%\*]/);
-    my $line = sprintf("%8s$D%10.3f$D%10.2f$D%10.2f$D%10.2f$D%15d$D%15d$D%9d$D%9d$D%9d$D%9d$D%s\n", @$columns);
-    Output(1, $line);
+   print to_json($Tree, {pretty => 1});
 }
-
-Output(1, "Done");
 
 # Main code ends here
 
@@ -111,15 +153,13 @@ __DATA__
     Options => {
 	url => "https://www.coingecko.com/en/coins/all",
 	table_index => 0,          # which table on the page are we extracting
-	user => 'crypto',
 	exchange => 'coingecko',
-	password => 'crypto',
 	verbosity => 2,
-        delimiter => " ",
+	delimiter => " ",
 	filter => undef,
-	schema => 0,
+    format => 'json', # or "flat"
+    schema => 0,
 	primary => 2,       # which column will contain the primary key
-	columns => ['COIN','PRICE','24H','7D','30D','MKT CAP','LIQUIDITY','DEVELOPER','COMMUNITY','PUBLIC INTEREST','TOTAL'],             # only extract columns with these names (in the table header)
 	version => "cgo.pl version_info: 1.0.0-75-g3373969 3373969 master 04/13/18-13:38:23",
 	end => 0
     }
